4c3bcd5ee63b03bee38320b39553b556def74ef5
JayveeG <jaybriel19@gmail.com>
2018-06-30 02:59:55 +0800

    hello mark

--- Login.php
+++ Login.php
@@ -74,7 +74,83 @@
 
 	}	
 
-echo "<script>alert('tangaina')</script>"
+
+	if(isset($_POST['submit2'])){
+		echo "WEWWEWE";
+		$fName = $_POST['first_name'];
+		$mName = $_POST['middle_name'];
+		$lName = $_POST['last_name'];
+		$bldg = $_POST['bldg'];
+		$street = $_POST['street'];
+		$city = $_POST['city'];
+		$region = $_POST['region'];
+		$contact = $_POST['contact'];
+		$username = $_POST['email'];
+		$password = $_POST['password'];
+		$type = $_POST['memType'];
+
+		$addQuery = $db->prepare("INSERT INTO buildingnumber (bldgNoName) VALUES (:bldg)");
+		$addQuery->bindParam(":bldg", $bldg);
+		$addQuery->execute();
+
+		$addQuery = $db->prepare("INSERT INTO city (cityName) VALUES (:city)");
+		$addQuery->bindParam(":city", $city);
+		$addQuery->execute();
+
+		$addQuery = $db->prepare("INSERT INTO street (streetName) VALUES (:street)");
+		$addQuery->bindParam(":street", $street);
+		$addQuery->execute();
+
+		$addQuery = $db->prepare("INSERT INTO region (regionName) VALUES (:region)");
+		$addQuery->bindParam(":region", $region);
+		$addQuery->execute();
+
+		$selectStreet = $db->prepare("SELECT streetID from street ORDER BY streetID DESC LIMIT 1");
+		$selectStreet->execute();
+		$rows = $selectStreet->fetch(PDO::FETCH_ASSOC);
+		$streetNo = $rows['streetID'];
+
+		$selectBldg = $db->prepare("SELECT bldgNoID from buildingnumber ORDER BY bldgNoID DESC LIMIT 1");
+		$selectBldg->execute();
+		$rows = $selectBldg->fetch(PDO::FETCH_ASSOC);
+		$bldgNo = $rows['bldgNoID'];
+
+		$selectCity = $db->prepare("SELECT cityID from city ORDER BY cityID DESC LIMIT 1");
+		$selectCity->execute();
+		$rows = $selectCity->fetch(PDO::FETCH_ASSOC);
+		$cityNo = $rows['cityID'];
+
+		$selectRegion = $db->prepare("SELECT regionID from region ORDER BY regionID DESC LIMIT 1");
+		$selectRegion->execute();
+		$rows = $selectRegion->fetch(PDO::FETCH_ASSOC);
+		$regionNo = $rows['regionID'];
+
+		$addQuery = $db->prepare("INSERT INTO address (bldgNoID, streetID, cityID, regionID) VALUES (:bldg,:street,:city,:region)");
+		$addQuery->bindParam(":bldg", $bldgNo);
+		$addQuery->bindParam(":street", $streetNo);
+		$addQuery->bindParam(":city", $cityNo);
+		$addQuery->bindParam(":region", $regionNo);
+		$addQuery->execute();
+
+		$selectAddress = $db->prepare("SELECT addressID from address ORDER BY addressID DESC LIMIT 1");
+		$selectAddress->execute();
+		$rows = $selectAddress->fetch(PDO::FETCH_ASSOC);
+		$addressNo = $rows['addressID'];
+
+		$addQuery = $db->prepare("INSERT INTO financialadvisor (fName, mName, lName, email,password, mobileNo,age, address,membershipType) VALUES (:firstName, :middleName, :lastName, :username, :password, :contact,20, :address,:membershipType)");
+		$addQuery->bindParam(":firstName", $fName);
+		$addQuery->bindParam(":middleName", $mName);
+		$addQuery->bindParam(":lastName", $lName);
+		$addQuery->bindParam(":username", $username);
+		$addQuery->bindParam(":password", $password);
+		$addQuery->bindParam(":address", $addressNo);
+		$addQuery->bindParam(":contact", $contact);
+		$addQuery->bindParam(":membershipType", $type);
+		$addQuery->execute();
+
+	}	
+
+
 ?>
 <head>
   <meta charset="utf-8">
@@ -153,8 +229,8 @@ font-weight: 300;
 						<a href="sign_up/user.php"><button class="btn waves-effect waves-light col s12" type="submit" name="action">CLIENT</button></a>
 					</div>
 					<div class="col s12 center">
-						<button class="btn waves-effect waves-light col s12" type="submit" name="action">	<a href="sign_up/fa.php"><button class="btn waves-effect waves-light col s12" type="submit" name="action">FINANCIAL ADVISER</button></a>
-                        </button>
+						<a href="sign_up/fa.php"><button class="btn waves-effect waves-light col s12" type="submit" name="action">FINANCIAL ADVISER</button></a>
+                        
 					</div>
 			  </div>
 				
--- sign_up/fa.php
+++ sign_up/fa.php
@@ -16,55 +16,60 @@
 <body>
 <div class="container">
     <div class="row">
-        <form class="col s12" id="reg-form">
+        <form class="col s12" action="../Login.php" method="post" id="reg-form">
             <h3>Financial Advisor
                 <i class="fa fa-address-card prefix" style="font-size:50px"></i></h3>
             <center> <small><disabled style="color: darkgray">your first step to finding the more clients in need of your expertise</disabled></small></center>
-            <div class="row">
+                <div class="row">
                 <div class="input-field col s4">
-                    <input id="first_name" type="text" class="validate" required>
+                    <input id="first_name" type="text" class="validate" required value="<?php if(isset($_POST['first_name'])) echo $_POST['first_name'];?>" name="first_name" >
                     <label for="first_name">First Name</label>
                 </div>
                 <div class="input-field col s4">
-                    <input id="middle_name" type="text" class="validate" required>
+                    <input id="middle_name" type="text" class="validate" required value="<?php if(isset($_POST['middle_name'])) echo $_POST['middle_name'];?>" name="middle_name" >
                     <label for="middle_name">Middle Name</label>
                 </div>
 
                 <div class="input-field col s4">
-                    <input id="last_name" type="text" class="validate" required>
+                    <input id="last_name" type="text" class="validate" required  value="<?php if(isset($_POST['last_name'])) echo $_POST['last_name'];?>" name="last_name">
                     <label for="last_name">Last Name</label>
                 </div>
 
                 <div class="input-field col s2">
-                    <input id="bldg" type="text" class="validate" required>
+                    <input id="bldg" type="text" class="validate" required  value="<?php if(isset($_POST['bldg'])) echo $_POST['bldg'];?>" name="bldg">
                     <label for="bldg">Bldg. #</label>
                 </div>
 
                 <div class="input-field col s4">
-                    <input id="street" type="text" class="validate" required>
+                    <input id="street" type="text" class="validate" required value="<?php if(isset($_POST['street'])) echo $_POST['street'];?>" name="street">
                     <label for="street">Street</label>
                 </div>
                 <div class="input-field col s3">
-                    <input id="city" type="text" class="validate" required>
+                    <input id="city" type="text" class="validate" required value="<?php if(isset($_POST['city'])) echo $_POST['city'];?>" name="city">
                     <label for="city">City</label>
                 </div>
                 <div class="input-field col s3">
-                    <input id="region" type="text" class="validate" required>
+                    <input id="region" type="text" class="validate" required value="<?php if(isset($_POST['region'])) echo $_POST['region'];?>" name="region">
                     <label for="region">Region</label>
                 </div>
                 <div class="input-field col s6">
 
-                    <input id="contact" type="text" class="validate" required>
+                    <input id="contact" type="text" class="validate" required value="<?php if(isset($_POST['contact'])) echo $_POST['contact'];?>" name="contact">
                     <label for="contact">Contact Number</label>
                 </div>
 
                 <div class="input-field col s6">
+                    <select name ="memType">
+                        <option value="" disabled selected>Choose your option</option>
+                        <option value="Regular">Regular </option>
+                        <option value="Associate">Associate </option>
+                        <option value="Certified">Certified </option>
+                    </select>
+                    <label>Membership Type</label>
+                </div>
 
-                    <input id="email" type="email" class="validate" required>
-                    <label for="email">Email</label>
             </div>
-            </div>
-            <div class = "row">
+                <div class = "row">
                 <div class = "file-field input-field col s6">
                     <div class = "btn" style="width: auto">
                         <span>BROWSE</span>
@@ -76,20 +81,20 @@
                     </div>
                 </div>
 
-                <div class="input-field col s6">
-                    <select>
-                        <option value="" disabled selected>Choose your option</option>
-                        <option value="1">Regular </option>
-                        <option value="2">Associate </option>
-                        <option value="3">Certified </option>
-                    </select>
-                    <label>Membership Type</label>
-                </div>
+            
             </div>
-            <div class="row">
+            <div class ="row">
+            <div class="input-field col s6">
 
+                    <input id="email" type="email" class="validate" required name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email'];?>">
+                    <label for="email">Email</label>
+            </div>
+        </div>
+            </div>
+            
+            <div class="row">
                 <div class="input-field col s6">
-                    <input id="password" type="password" class="validate" minlength="6" required>
+                    <input id="password" type="password" class="validate" minlength="6" required value="<?php if(isset($_POST['password'])) echo $_POST['password'];?>" name="password">
                     <label for="password">Password</label>
                 </div>
                 <div class="input-field col s6">
@@ -99,7 +104,7 @@
             </div>
                 <div class="input-field col s12">
                     <center>
-                    <button class="btn btn-large btn-register waves-effect waves-light" type="submit" name="action">Register
+                    <button class="btn btn-large btn-register waves-effect waves-light" type="submit" name="submit2">Register
                     </button>
                     </center>
                 </div>
